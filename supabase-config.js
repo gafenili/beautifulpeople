e773ddf4a741e1a90a773badd6f8ca39f00fe5b2
gafenili <fenililopes@gmail.com>
2026-07-13 12:43:04 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // Cole aqui os dados do seu projeto Supabase (Settings > API).
 // Use somente a Publishable key (ou a anon key em projetos antigos).
 // Nunca coloque uma secret key ou service_role key neste arquivo.
-window.SUPABASE_URL = 'COLE_A_URL_DO_PROJETO_AQUI';
-window.SUPABASE_PUBLISHABLE_KEY = 'COLE_A_PUBLISHABLE_KEY_AQUI';
+window.SUPABASE_URL = "https://znvbnnbqybjxkwexqdn.supabase.co"
+window.SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpudmJubmJxeWJqeGtrd2V4cWRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3Nzc0NDUsImV4cCI6MjA5OTM1MzQ0NX0.yhHmbeOx-VzB4rMONrBbfGnIYaxR_kdsifnzGtefp8Y;"; 
